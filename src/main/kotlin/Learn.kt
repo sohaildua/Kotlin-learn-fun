dabeee2ade63a29c679c15ec0a9f30c11011e70d
dua <sohail.dua@webfleet.com>
2024-03-09 20:44:57 +0100

    learn kotlin variables and strings

---
+++ src/main/kotlin/Learn.kt
@@ -0,0 +1,29 @@
+package org.example
+
+class Learn {
+
+}
+
+fun main(args: Array<String>) {
+    println("123")
+    // // val and var
+    val name = "Bulbasaur"
+    println(name)
+    // val is immutable
+    var favFood = "Pizza"
+    println(favFood)
+    favFood= "Pasta"
+    println(favFood)
+    // var is immutable
+
+    println("favfood : ${favFood.length}")
+
+    // multiline
+    val makeItMultiline= """
+        oh my god
+        yay
+    """.trimIndent()
+
+    println(makeItMultiline)
+
+}
